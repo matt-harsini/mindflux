a27675eff5b732c9564600ced1f3ee474fb2dcf4
mk975025 <matthewkimcs@gmail.com>
2023-05-31 15:45:16 -0700

    able to get the emoji to move

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { Feelings } from "../shared/interfaces";
+import { useState, useRef } from "react";
 import { InputProps } from "../shared/types";
 import { motion } from "framer-motion";
+import { Feelings } from "../shared/interfaces";
 
 const feelings: Feelings = {
   CARD_HAPPY: "Happiness",
@@ -12,23 +12,26 @@ const feelings: Feelings = {
 
 export default function Input({ card_title }: InputProps) {
   const [input, setInput] = useState("1");
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0 });
+  const emoji = useRef<HTMLDivElement>(null);
 
   function changeInput(e: React.MouseEvent) {
     if (!(e.target instanceof HTMLElement)) return;
     if (e.target.nodeName === "UL") return;
+    if (!emoji.current) return;
 
     const { innerText } = e.target as HTMLElement;
-    const emoji = document.getElementById(
-      `${feelings[card_title]}-emoji`
-    ) as HTMLElement;
-
-    const target = emoji
-      .closest("ul")
+    const target = emoji.current
+      ?.closest("ul")
       ?.querySelector(`[data-value='${innerText}']`) as HTMLElement;
 
     const targetDimensions = target.getBoundingClientRect();
-    setTarget(targetDimensions);
+    const emojiDimensions: DOMRect = emoji.current.getBoundingClientRect();
+    console.log(targetDimensions);
+
+    setPosition({
+      x: targetDimensions.left - emojiDimensions.left,
+    });
     setInput(innerText);
   }
 
@@ -43,12 +46,13 @@ export default function Input({ card_title }: InputProps) {
           className="bg-base-300 rounded-full text-xl cursor-pointer w-[64px] h-[64px] lg:w-[96px] lg:h-[96px] flex items-center justify-center relative"
         >
           <span>1</span>
-          <div
-            id={`${feelings[card_title]}-emoji`}
-            className="flex items-center justify-center w-full h-full absolute transition-transform duration-1000 pointer-events-none"
+          <motion.div
+            ref={emoji}
+            animate={position}
+            className="z-30 flex items-center justify-center w-full h-full absolute transition-transform duration-1000 pointer-events-none"
           >
-            <motion.span className="fa-solid fa-face-smile scale-[2.1] lg:scale-[3.7] text-accent" />
-          </div>
+            <span className="fa-solid fa-face-smile scale-[2.1] lg:scale-[3.7] text-accent" />
+          </motion.div>
         </li>
         <li
           data-value="2"
