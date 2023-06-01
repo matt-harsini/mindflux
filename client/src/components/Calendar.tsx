3597a296b24cab9de1ba896d9c13439021548b75
mk975025 <matthewkimcs@gmail.com>
2023-05-31 23:01:39 -0700

    changed all colors

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -136,7 +136,7 @@ function classNames(...classes: (string | boolean | undefined)[]) {
 export default function Example() {
   return (
     <div className="lg:flex lg:h-full lg:flex-col">
-      <header className="flex items-center justify-between border-b border-gray-200 px-6 py-4 lg:flex-none">
+      <header className="flex items-center justify-between border-base-300 px-6 py-4 lg:flex-none bg-base-200">
         <h1 className="font-semibold leading-6 text-primary-content text-2xl">
           <time dateTime="2022-01">January 2022</time>
         </h1>
@@ -366,38 +366,38 @@ export default function Example() {
         </div>
       </header>
       <div className="shadow ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
-        <div className="grid grid-cols-7 gap-px border-b border-gray-300 bg-gray-200 text-center text-xs font-semibold leading-6 text-gray-700 lg:flex-none">
-          <div className="bg-white py-2">
+        <div className="grid grid-cols-7 gap-px border-b border-base-300 text-center text-xs font-semibold leading-6 text-primary-content lg:flex-none bg-base-300">
+          <div className="bg-base-100 py-2">
             M<span className="sr-only sm:not-sr-only">on</span>
           </div>
-          <div className="bg-white py-2">
+          <div className="bg-base-100 py-2">
             T<span className="sr-only sm:not-sr-only">ue</span>
           </div>
-          <div className="bg-white py-2">
+          <div className="bg-base-100 py-2">
             W<span className="sr-only sm:not-sr-only">ed</span>
           </div>
-          <div className="bg-white py-2">
+          <div className="bg-base-100 py-2">
             T<span className="sr-only sm:not-sr-only">hu</span>
           </div>
-          <div className="bg-white py-2">
+          <div className="bg-base-100 py-2">
             F<span className="sr-only sm:not-sr-only">ri</span>
           </div>
-          <div className="bg-white py-2">
+          <div className="bg-base-100 py-2">
             S<span className="sr-only sm:not-sr-only">at</span>
           </div>
-          <div className="bg-white py-2">
+          <div className="bg-base-100 py-2">
             S<span className="sr-only sm:not-sr-only">un</span>
           </div>
         </div>
         <div className="flex bg-gray-200 text-xs leading-6 text-primary-content lg:flex-auto">
-          <div className="hidden w-full lg:grid lg:grid-cols-7 lg:grid-rows-6 lg:gap-px">
+          <div className="hidden w-full lg:grid lg:grid-cols-7 lg:grid-rows-6 lg:gap-px bg-base-300">
             {days.map((day) => (
               <div
                 key={day.date}
                 className={classNames(
                   day.isCurrentMonth
-                    ? "bg-base-300"
-                    : "bg-base-100 text-gray-500",
+                    ? "bg-base-100"
+                    : "bg-base-200 text-gray-200",
                   "relative px-3 py-2"
                 )}
               >
@@ -405,7 +405,7 @@ export default function Example() {
                   dateTime={day.date}
                   className={
                     day.isToday
-                      ? "flex h-6 w-6 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white"
+                      ? "flex h-6 w-6 items-center justify-center rounded-full bg-accent font-semibold text-white"
                       : undefined
                   }
                 >
@@ -416,12 +416,12 @@ export default function Example() {
                     {day.events.slice(0, 2).map((event) => (
                       <li key={event.id}>
                         <a href={event.href} className="group flex">
-                          <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
+                          <p className="flex-auto truncate font-medium text-primary-content group-hover:text-indigo-600">
                             {event.name}
                           </p>
                           <time
                             dateTime={event.datetime}
-                            className="ml-3 hidden flex-none text-gray-500 group-hover:text-indigo-600 xl:block"
+                            className="ml-3 hidden flex-none text-gray-300 group-hover:text-indigo-600 xl:block"
                           >
                             {event.time}
                           </time>
