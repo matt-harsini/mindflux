49cba15acadc47dc91ce62029bd774c978552aa1
mk975025 <matthewkimcs@gmail.com>
2023-06-04 17:40:45 -0700

    fixed a few bugs

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,11 +1,11 @@
 import { User } from "../models/userModel.js";
 
-function login() {
+function login(req, res) {
   console.log(123);
   res.json({ msg: "Login User" });
 }
 
-function register() {
+function register(req, res) {
   res.json({ msg: "Login User" });
 }
 
