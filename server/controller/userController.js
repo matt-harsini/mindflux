29203359af17eec9e3d830e6dff18044b505e432
mk975025 <matthewkimcs@gmail.com>
2023-06-05 17:02:27 -0700

    signing jwts

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,11 @@
+import "dotenv/config";
+import jwt from "jsonwebtoken";
 import StatusCode from "http-status-codes";
 import { User } from "../models/userModel.js";
+
+function createToken(_id) {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+}
 
 function login(req, res) {
   console.log(123);
@@ -10,7 +16,9 @@ async function register(req, res) {
   const { email, password } = req.body;
   try {
     const user = await User.register(email, password);
-    res.status(StatusCode.OK).json({ email, password });
+    const token = createToken(user._id);
+    console.log(token);
+    res.status(StatusCode.OK).json({ email, token });
   } catch (error) {
     res.status(StatusCode.BAD_REQUEST).json({ error: error.message });
   }
