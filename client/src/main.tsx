b7d6543744982d0746eb9d1843d8500e656f827c
mk975025 <matthewkimcs@gmail.com>
2023-06-05 19:45:28 -0700

    setting up react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
