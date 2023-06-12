340f8635e2dd8abc7635b2d0d2b0810531b33de8
mk975025 <matthewkimcs@gmail.com>
2023-06-11 23:15:47 -0700

    quick tweak

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,14 +1,4 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 import { useAuthContext } from "../hooks/useAuthContext";
-const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
-
-const renderLineChart = (
-  <LineChart width={600} height={300} data={data}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <XAxis dataKey="name" />
-    <YAxis />
-  </LineChart>
-);
 
 export default function Dashboard() {
   const { username } = useAuthContext();
