dd1aa8ff3a3bb4df7c50a8581f3fa19024a85ce3
mk975025 <matthewkimcs@gmail.com>
2023-06-12 22:20:50 -0700

    css tweaks

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -49,27 +49,14 @@ export default function Login() {
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h4 className="text-3xl font-bold text-accent">mindflux</h4>
       <div
-        className={`max-w-max alert alert-error justify-items-center mt-1.5 py-2.5 ${
+        className={`max-w-max alert alert-error flex justify-items-center mt-1 py-2.5 ${
           !isError && "invisible"
         }`}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="stroke-current shrink-0 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-        <span>{error?.response.data.error}</span>
+        <span className="text-center">{error?.response.data.error}</span>
       </div>
       <form
-        className="bg-neutral shadow-md rounded py-12 px-8 flex flex-col gap-8 max-w-md mt-2.5"
+        className="bg-neutral shadow-md rounded py-12 px-8 flex flex-col gap-8 max-w-md mt-1"
         onSubmit={handleSubmit}
       >
         <div>
@@ -109,7 +96,7 @@ export default function Login() {
           </button>
         </div>
       </form>
-      <Link to="/register" className="text-accent text-md mt-5">
+      <Link to="/register" className="text-white text-md mt-5">
         Don't have an account? Sign up
       </Link>
     </div>
