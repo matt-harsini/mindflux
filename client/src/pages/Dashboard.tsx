f7c9bd42c55af8e87df0fbee2f99ecd61e307b8c
mk975025 <matthewkimcs@gmail.com>
2023-06-16 20:43:02 -0700

    css tweaks

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -123,7 +123,7 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="flex flex-col gap-y-12">
+      <div className="flex flex-col gap-y-10">
         <h3 className="text-2xl text-center sm:text-3xl mx-auto lg:mx-0 text-primary-content lg:text-4xl font-bold mb-7 sm:text-start">
           Hello {username}
         </h3>
@@ -141,7 +141,7 @@ export default function Dashboard() {
             All
           </button>
         </div>
-        <div className="flex flex-col gap-y-4">
+        <div className="flex flex-col gap-y-2">
           <h4 className="text-lg text-white text-center">Guide</h4>
           <div className="flex items-center justify-evenly bg-base-200 bg-opacity-75 rounded-xl">
             {Object.keys(inputIcons).map((icon) => {
