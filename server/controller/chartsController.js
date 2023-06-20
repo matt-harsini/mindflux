07548ce9ea5c6becf78b18368185489e7caca70c
mk975025 <matthewkimcs@gmail.com>
2023-06-20 12:45:47 -0700

    working on aggregation pipeline

--- server/controller/chartsController.js
+++ server/controller/chartsController.js
@@ -1,9 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 import { Log } from "../models/logModel.js";
-import mongoose from "mongoose";
 
-const ObjectId = mongoose.Types.ObjectId;
-
+const dates = [new Date().setDate()];
+const sevenDaysAgo = new Date();
+sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
 async function getChartData(req, res) {
   const { f, l } = req.query;
   const { _id: user_id } = req.user;
@@ -21,17 +21,43 @@ async function getChartData(req, res) {
             },
             {
               date: {
-                $gte: f,
-                $lte: l,
+                $gte: sevenDaysAgo,
               },
             },
           ],
+        },
+      },
+      {
+        $project: {
+          name: new Date().toISOString(),
+          createdAt: "$createdAt",
+          Happiness: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_HAPPY", 0],
+            },
+          },
+          Sadness: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_SAD", 0],
+            },
+          },
+          Anger: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_ANGRY", 0],
+            },
+          },
+          Anxiety: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_ANXIOUS", 0],
+            },
+          },
         },
       },
     ]);
     console.log(documents);
     return res.status(StatusCodes.OK).json({ documents });
   } catch (error) {
+    console.log(error);
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: error });
