709ec4590cf3802f77a9ee498b1516863824f680
mk975025 <matthewkimcs@gmail.com>
2023-06-20 13:39:30 -0700

    revert

--- server/controller/chartsController.js
+++ server/controller/chartsController.js
@@ -1,51 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import { Log } from "../models/logModel.js";
 
-const pipeline = [
-  {
-    $match: {
-      $and: [
-        {
-          user_id: user_id.toString(),
-        },
-        {
-          date: {
-            $gte: f,
-            $lte: l,
-          },
-        },
-      ],
-    },
-  },
-  {
-    $group: {
-      _id: {
-        $substr: ["$createdAt", 0, 10],
-      },
-      Happiness: {
-        $sum: {
-          $ifNull: ["$moodMeter.CARD_HAPPY", 0],
-        },
-      },
-      Sadness: {
-        $sum: {
-          $ifNull: ["$moodMeter.CARD_SAD", 0],
-        },
-      },
-      Anger: {
-        $sum: {
-          $ifNull: ["$moodMeter.CARD_ANGRY", 0],
-        },
-      },
-      Anxiety: {
-        $sum: {
-          $ifNull: ["$moodMeter.CARD_ANXIOUS", 0],
-        },
-      },
-    },
-  },
-];
-
 async function getChartData(req, res) {
   const { f, l } = req.query;
   const { _id: user_id } = req.user;
@@ -54,7 +9,50 @@ async function getChartData(req, res) {
       const documents = await Chart.find({});
       return res.status(StatusCodes.OK).json({ documents });
     }
-    const documents = await Log.aggregate(pipeline);
+    const documents = await Log.aggregate([
+      {
+        $match: {
+          $and: [
+            {
+              user_id: user_id.toString(),
+            },
+            {
+              date: {
+                $gte: f,
+                $lte: l,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $substr: ["$createdAt", 0, 10],
+          },
+          Happiness: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_HAPPY", 0],
+            },
+          },
+          Sadness: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_SAD", 0],
+            },
+          },
+          Anger: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_ANGRY", 0],
+            },
+          },
+          Anxiety: {
+            $sum: {
+              $ifNull: ["$moodMeter.CARD_ANXIOUS", 0],
+            },
+          },
+        },
+      },
+    ]);
     console.log(documents);
     return res.status(StatusCodes.OK).json({ documents });
   } catch (error) {
