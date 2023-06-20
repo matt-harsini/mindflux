b9e9fffadaabc2482f02a85f7dfe9ea6e7349512
mk975025 <matthewkimcs@gmail.com>
2023-06-19 21:51:57 -0700

    added animations for calendar page

--- client/src/pages/CalendarPage.tsx
+++ client/src/pages/CalendarPage.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Calendar } from "../components";
 
 export default function CalendarPage() {
@@ -6,9 +7,21 @@ export default function CalendarPage() {
       <h3 className="text-2xl text-center sm:text-3xl mx-auto lg:mx-0 text-primary-content lg:text-4xl font-bold mb-7 sm:text-start">
         Here is your month
       </h3>
-      <div className="flex flex-col mx-auto gap-10">
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+          },
+          visible: {
+            opacity: 1,
+          },
+        }}
+        initial="hidden"
+        animate="visible"
+        className="flex flex-col mx-auto gap-10"
+      >
         <Calendar />
-      </div>
+      </motion.div>
     </>
   );
 }
