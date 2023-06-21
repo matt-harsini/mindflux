c3d583d2ff03dfa28cad5b5ec7ad461092a9335d
mk975025 <matthewkimcs@gmail.com>
2023-06-21 16:44:23 -0700

    refactoring code

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -19,7 +19,7 @@ async function auth(req, res, next) {
     req.user = await User.findOne({ _id }).select("_id");
     next();
   } catch (error) {
-    res
+    return res
       .status(StatusCodes.UNAUTHORIZED)
       .json({ error: "Request is not authorized" });
   }
