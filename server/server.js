09724441f8030b63effac94951fd4ac20f59bd2e
mk975025 <matthewkimcs@gmail.com>
2023-06-27 15:47:32 -0700

    configuring cors

--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ import cors from "cors";
 import helmet from "helmet";
 import { createAPIError, handleError } from "./errors/errorHandler.js";
 import { StatusCodes } from "http-status-codes";
+import { corsOptions } from "./config.js";
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 app.options("*", cors());
 const limiter = rateLimit({
   max: 150,
