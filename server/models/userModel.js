96bef635d3a84ec1c9d1a6daed0e9594efa04e3d
mk975025 <matthewkimcs@gmail.com>
2023-06-27 23:01:33 -0700

    updated regex

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -41,7 +41,7 @@ const userSchema = new Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /\d{3}-\d{3}-\d{4}/.test(v);
+        return /^\d{3}-\d{3}-\d{4}$/.test(v);
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
