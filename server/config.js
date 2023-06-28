e9667935fbe42a5b5fcd7f39819c73b81b577fb2
mk975025 <matthewkimcs@gmail.com>
2023-06-27 17:04:52 -0700

    configuring allowed origins

--- server/config.js
+++ server/config.js
@@ -1,7 +1,4 @@
-export const allowedOrigins = [
-  "https://mindflux.onrender.com",
-  "http://localhost:5173",
-];
+export const allowedOrigins = ["https://mindflux.onrender.com"];
 
 export const corsOptions = {
   origin: (origin, callback) => {
