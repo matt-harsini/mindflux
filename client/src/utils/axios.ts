39b70f000f250a8e34af697504b7dab9d20c9ea0
mk975025 <matthewkimcs@gmail.com>
2023-07-02 21:40:17 -0700

    specifying baseurls

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const authFetch = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://mindflux.fly.dev/api",
 });
 
 export const getAuthFetch = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://mindflux.fly.dev/api",
 });
