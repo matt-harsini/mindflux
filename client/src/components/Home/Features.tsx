0662383db819f11896b062929773eb1f562db9c4
matthew <matthewkimcs@gmail.com>
2023-07-23 22:53:44 -0700

    changing image

--- client/src/components/Home/Features.tsx
+++ client/src/components/Home/Features.tsx
@@ -28,7 +28,7 @@ export default function Features() {
   return (
     <div className="overflow-hidden bg-base-100 py-24 sm:py-32 max-w-7xl mx-auto">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid items-center max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 text-accent">
