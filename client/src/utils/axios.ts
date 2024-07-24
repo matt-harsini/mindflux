552e709285e71ebc2cd2a8bd47a337ff5f3fa525
Matthew Kim <matthewkim@Matthews-MBP.lan>
2024-07-23 18:23:37 -0700

    update backend endpoints

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 //https://mindflux.fly.dev/api
 //http://localhost:4000/api
 export const authFetch = axios.create({
-  baseURL: "https://server-aged-moon-5092.fly.dev",
+  baseURL: "https://server-aged-moon-5092.fly.dev/api",
 });
 
 export const getAuthFetch = axios.create({
-  baseURL: "https://server-aged-moon-5092.fly.dev",
+  baseURL: "https://server-aged-moon-5092.fly.dev/api",
 });
