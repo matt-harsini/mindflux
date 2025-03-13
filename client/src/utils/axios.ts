3f0ed60212d4a31a604db6561a9a7462323b1bb0
matt-harsini <matthewkimcs@gmail.com>
2025-03-12 20:30:45 -0700

    remove lock

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 //https://mindflux.fly.dev/api
 //http://localhost:4000/api
 export const authFetch = axios.create({
-  baseURL: "https://server-morning-dream-3737.fly.dev",
+  baseURL: "https://server-morning-dream-3737.fly.dev/api",
 });
 
 export const getAuthFetch = axios.create({
-  baseURL: "https://server-morning-dream-3737.fly.dev",
+  baseURL: "https://server-morning-dream-3737.fly.dev/api",
 });
